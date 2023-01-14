fa6c657a6ac2ec32dc7aa62c265bae9327594c1e
Jack Walmsley <jackawalmsley@gmail.com>
2023-01-14 17:22:27 -0500

    IPFS uploading working

--- ui/submit.js
+++ ui/submit.js
@@ -5,34 +5,37 @@
 //         testResults(event)
 //     });
 function testResults(e) {
+    document.getElementById("result").innerText = "Loading...";
     var entry = {};
     entry.name = e.form.name.value;
     entry.score = e.form.score.value;
     entry.blurb = e.form.blurb.value;
     console.log(entry);
 
-    // const form = new FormData();
-    // form.append(
-    //     'filePath',
-    //     'data:application/json;name=' +
-    //         entry.name +
-    //         '.json;base64,' +
-    //         btoa(JSON.stringify(entry))
-    // );
 
-    // const options = {
-    //     method: 'POST',
-    //     headers: {
-    //         accept: 'application/json',
-    //         'X-API-Key': 'sk_live_603155cd-2bb5-4e83-a52d-7a1772539a1a',
-    //     },
-    // };
+    let file = new File([JSON.stringify(entry)],entry.name+".json")
+    const form = new FormData();
+    form.append(
+        'filePath',file
+        
+    );
 
-    // options.body = form;
+    const options = {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'X-API-Key': 'sk_live_603155cd-2bb5-4e83-a52d-7a1772539a1a',
+        },
+    };
 
-    // fetch('https://api.verbwire.com/v1/nft/store/file', options)
-    //     .then((response) => response.json())
-    //     .then((response) => console.log(response))
-    //     .catch((err) => console.error(err));
+    options.body = form;
+
+    fetch('https://api.verbwire.com/v1/nft/store/file', options)
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response)
+            document.getElementById("result").innerText = "The IPFS location of your data is: " + response.ipfs_storage.ipfs_url.split("/ipfs/")[1]
+        })
+        .catch((err) => console.error(err));
     return false;
 }
